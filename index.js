a023f93343762bc6fd6e7a15ac5e0d2230625124
Emily Harbeck <eharbeck@me.com>
2024-09-04 15:52:44 +1000

    passed all tests

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
-// Write your solution here!
+const cats = ["Milo","Otis","Garfield"];
+
+function destructivelyAppendCat(name){
+    cats.push(name)
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift(name)
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop()
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift()
+}
+
+function appendCat(name) {
+    const newCats = [...cats];
+    newCats.push(name);
+    return newCats;
+    
+}
+
+function prependCat(name) {
+    const moreCats = [...cats];
+    moreCats.unshift(name);
+    return moreCats;
+}
+
+function removeLastCat() {
+    const lessCats = [...cats];
+    lessCats.pop(name);
+    return lessCats;
+}
+
+function removeFirstCat() {
+    const oneLessCats = [...cats];
+    oneLessCats.shift(name);
+    return oneLessCats;
+}
